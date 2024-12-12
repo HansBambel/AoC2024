6b7cc0c773cd357eafdd55cfb26aaa9f84030562
HansBambel <Kevin.Trebing@gmx.net>
2024-12-12 23:59:45 +0100

    Day 12 - part 2 optimization

--- aoc/day12/day12.py
+++ aoc/day12/day12.py
@@ -4,35 +4,6 @@ input_data = [[]]
 seen = set()
 
 
-def count_size_and_edges_of_area(pos: tuple[int, int], letter: str) -> tuple[int, int]:
-    global input_data
-    global seen
-    seen.add(pos)
-    edges = 0
-    size = 1
-    for y, x in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        new_pos = (pos[0] + y, pos[1] + x)
-        if (
-            new_pos[0] < 0
-            or new_pos[1] < 0
-            or new_pos[0] >= len(input_data)
-            or new_pos[1] >= len(input_data[0])
-        ):
-            edges += 1
-            continue
-        if input_data[new_pos[0]][new_pos[1]] == letter:
-            if new_pos in seen:
-                continue
-            new_size, new_edges = count_size_and_edges_of_area(
-                (new_pos[0], new_pos[1]), letter
-            )
-            edges += new_edges
-            size += new_size
-        else:
-            edges += 1
-    return size, edges
-
-
 def count_size_and_sides_of_area(
     pos: tuple[int, int], letter: str
 ) -> tuple[int, set[tuple[int, int, str]]]:
@@ -74,8 +45,9 @@ def part_1(input_file: str):
     for y, line in enumerate(input_data):
         for x, letter in enumerate(line):
             if (y, x) not in seen:
-                size, edges = count_size_and_edges_of_area((y, x), letter)
-                total += size * edges
+                size, edges = count_size_and_sides_of_area((y, x), letter)
+                # Only use the length of the edges
+                total += size * len(edges)
     return total
 
 
