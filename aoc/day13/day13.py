a6c80d9ea61d7165e56b32518886f52ed8761213
HansBambel <Kevin.Trebing@gmx.net>
2024-12-13 09:12:50 +0100

    Day 13 - part 2 (math, yo)

--- aoc/day13/day13.py
+++ aoc/day13/day13.py
@@ -4,26 +4,41 @@ from pathlib import Path
 machines = []
 
 
-def calc_tokens(machine: list[str]) -> int:
-    solutions = []
+def calc_tokens(machine: list[str], part2) -> int:
     a = list(map(int, re.findall(r"\d+", machine[0])))
     b = list(map(int, re.findall(r"\d+", machine[1])))
     prize = list(map(int, re.findall(r"\d+", machine[2])))
-    for i in range(101):
-        for j in range(101):
-            if a[0] * i + b[0] * j == prize[0] and a[1] * i + b[1] * j == prize[1]:
-                solutions.append(i * 3 + j * 1)
-    return max(solutions) if solutions else 0
+    if part2:
+        prize = [prize[0] + 10000000000000, prize[1] + 10000000000000]
+
+    # prize[0] = a[0]*a_pressed+b[0]*b_pressed
+    # prize[1] = a[1]*a_pressed+b[1]*b_pressed
+
+    # p0 - b[0]*b_pressed = a[0]*a_pressed
+    # (p0 - b[0]*b_pressed) / a[0] = a_pressed
+
+    # p1 - a[1]*a_pressed = b[1]*b_pressed
+    # (p1 - a[1]*a_pressed) / b[1] = b_pressed
+
+    # (p1 - a[1]*((p0 - b[0]*b_pressed) / a[0])) / b[1] = b_pressed
+    # and so on
+
+    b_pressed = (prize[1] * a[0] - a[1] * prize[0]) / (a[0] * b[1] - a[1] * b[0])
+    a_pressed = (prize[0] - b[0] * b_pressed) / a[0]
+    if b_pressed.is_integer() and a_pressed.is_integer():
+        return int(a_pressed * 3 + b_pressed * 1)
+    else:
+        return 0
 
 
-def part_1(input_file: str):
+def part_1(input_file: str, part2=False):
     global machines
     data_file = Path(__file__).with_name(input_file).read_text()
     machines = data_file.split("\n\n")
     machines = [machine.split("\n") for machine in machines]
     tokens = 0
     for machine in machines:
-        tokens += calc_tokens(machine)
+        tokens += calc_tokens(machine, part2=part2)
 
     return tokens
 
@@ -45,9 +60,5 @@ if __name__ == "__main__":
 
     # #### Part 2 ####
     print("#" * 10 + " Part 2 " + "#" * 10)
-    result = part_2("input_ex.txt")
-    print(result)
-    assert result == 1337
-
-    result = part_2("input.txt")
+    result = part_1("input.txt", part2=True)
     print(result)
