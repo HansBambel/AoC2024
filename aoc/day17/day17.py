bf815819287f4d07d1f64a9002ecc3d528886de2
HansBambel <Kevin.Trebing@gmx.net>
2024-12-17 09:02:51 +0100

    Day 17 - Part 1 cleanup

--- aoc/day17/day17.py
+++ aoc/day17/day17.py
@@ -4,17 +4,7 @@ from pathlib import Path
 input_data = []
 
 
-def part_1(input_file: str):
-    global input_data
-    data_file = Path(__file__).with_name(input_file).read_text()
-    input_data = data_file.split("\n\n")
-    reg_a, reg_b, reg_c = input_data[0].split("\n")
-    reg_a = int(reg_a.split()[-1])
-    reg_b = int(reg_b.split()[-1])
-    reg_c = int(reg_c.split()[-1])
-    program = list(re.findall(r"\d+", input_data[1]))
-    program = [int(x) for x in program]
-
+def run_operations(program, reg_a, reg_b, reg_c):
     ind = 0
     out = []
     while ind < len(program):
@@ -31,11 +21,11 @@ def part_1(input_file: str):
                 raise ValueError("Invalid operand")
             case _:
                 combo = operand
+
         match instr:
             case 0:
                 reg_a = int(reg_a / 2**combo)
             case 1:
-                # TODO maybe check
                 reg_b = reg_b ^ operand
             case 2:
                 reg_b = combo % 8
@@ -52,6 +42,21 @@ def part_1(input_file: str):
             case 7:
                 reg_c = int(reg_a / 2**combo)
         ind += 2
+    return out
+
+
+def part_1(input_file: str):
+    global input_data
+    data_file = Path(__file__).with_name(input_file).read_text()
+    input_data = data_file.split("\n\n")
+    reg_a, reg_b, reg_c = input_data[0].split("\n")
+    reg_a = int(reg_a.split()[-1])
+    reg_b = int(reg_b.split()[-1])
+    reg_c = int(reg_c.split()[-1])
+    program = list(re.findall(r"\d+", input_data[1]))
+    program = [int(x) for x in program]
+
+    out = run_operations(program, reg_a, reg_b, reg_c)
     return ",".join(map(str, out))
 
 
@@ -59,6 +64,23 @@ def part_2(input_file: str):
     global input_data
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n\n")
+    _, reg_b, reg_c = input_data[0].split("\n")
+    reg_b = int(reg_b.split()[-1])
+    reg_c = int(reg_c.split()[-1])
+    program = list(re.findall(r"\d+", input_data[1]))
+    program = [int(x) for x in program]
+
+    reg_a = 0
+    out = ""
+    while out != program:
+        reg_a += 1
+        if reg_a % 100000 == 0:
+            print(reg_a)
+        try:
+            out = run_operations(program, reg_a, reg_b, reg_c)
+        except ValueError:
+            continue
+    return reg_a
 
 
 if __name__ == "__main__":
@@ -72,9 +94,9 @@ if __name__ == "__main__":
 
     # #### Part 2 ####
     print("#" * 10 + " Part 2 " + "#" * 10)
-    result = part_2("input_ex.txt")
+    result = part_2("input_ex2.txt")
     print(result)
-    assert result == 1337
+    assert result == 117440
 
     result = part_2("input.txt")
     print(result)
