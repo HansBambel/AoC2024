c61e45e758cfdc9fe5cbeb08723c5b0cfe61a812
HansBambel <Kevin.Trebing@gmx.net>
2024-12-18 09:57:14 +0100

    Day 18 - part 1

---
+++ aoc/day18/day18.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from queue import PriorityQueue
+
+input_data = []
+
+
+def find_path(start_pos, end_pos, blocked):
+    seen = set()
+    prev = {}
+    dist = {start_pos: 0}
+    queue = PriorityQueue()
+    queue.put((0, start_pos))
+    while not queue.empty():
+        _, next_move = queue.get()
+        seen.add(next_move)
+        x, y = next_move
+        # if next_move == goal:
+        #     break
+        options: list[tuple[int, int]] = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        for dir in options:
+            new_pos = (x + dir[0], y + dir[1])
+            new_score = dist[next_move] + 1
+            if new_pos in blocked:
+                continue
+            if new_pos in seen:
+                continue
+            if (
+                new_pos[0] < 0
+                or new_pos[1] < 0
+                or new_pos[0] >= end_pos[0] + 1
+                or new_pos[1] >= end_pos[1] + 1
+            ):
+                continue
+
+            if dist.get(new_pos, 1e9) > new_score:
+                dist[new_pos] = new_score
+                prev[new_pos] = next_move
+                queue.put((new_score, new_pos))
+            # elif new_score == dist[new_pos]:
+            #     prev[new_pos].append(next_move)
+    return dist, prev
+
+
+def get_path(prev, end_pos):
+    path = []
+    while end_pos in prev:
+        path.append(end_pos)
+        end_pos = prev[end_pos]
+    return path
+
+
+def part_1(input_file: str, end_pos=(6, 6), start_after=1024):
+    global input_data
+    data_file = Path(__file__).with_name(input_file).read_text()
+    input_data = data_file.split("\n")
+    input_data = [list(map(int, row.split(","))) for row in input_data]
+    start_pos = (0, 0)
+    blocked = [tuple(pos) for pos in input_data[:start_after]]
+    dist, prev = find_path(start_pos, end_pos, blocked)
+
+    path = get_path(prev, end_pos)
+    for y in range(end_pos[1] + 1):
+        for x in range(end_pos[0] + 1):
+            if (x, y) in path:
+                print("O", end="")
+            elif (x, y) in blocked:
+                print("#", end="")
+            else:
+                print(".", end="")
+        print()
+    return dist[end_pos]
+
+
+def part_2(input_file: str):
+    global input_data
+    data_file = Path(__file__).with_name(input_file).read_text()
+    input_data = data_file.split("\n")
+
+
+if __name__ == "__main__":
+    print("#" * 10 + " Part 1 " + "#" * 10)
+    result_ex = part_1("input_ex.txt", end_pos=(6, 6), start_after=12)
+    print(result_ex)
+    assert result_ex == 22
+
+    result = part_1("input.txt", end_pos=(70, 70), start_after=1024)
+    print(result)
+
+    # #### Part 2 ####
+    print("#" * 10 + " Part 2 " + "#" * 10)
+    result = part_2("input_ex.txt")
+    print(result)
+    assert result == 1337
+
+    result = part_2("input.txt")
+    print(result)
