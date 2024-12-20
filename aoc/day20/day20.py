bb1f6eff7f8734d7f2c42475f37672b6e360a8e1
HansBambel <Kevin.Trebing@gmx.net>
2024-12-20 16:27:37 +0100

    Day 20 - part 1 cleaner?

--- aoc/day20/day20.py
+++ aoc/day20/day20.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 from queue import PriorityQueue
 
@@ -49,7 +50,7 @@ def get_pos(input_data: list[list[str]], character: str = "S") -> tuple[int, int
                 return y, x
 
 
-def part_1(input_file: str, threshold: int = 100):
+def part_1(input_file: str, threshold: int = 100, cheat_size=2):
     global input_data
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
@@ -57,43 +58,53 @@ def part_1(input_file: str, threshold: int = 100):
 
     start_pos = get_pos(input_data, "S")
     end_pos = get_pos(input_data, "E")
-    dist_base, _ = find_path(start_pos, end_pos)
-    base_line = dist_base[end_pos]
+    dist_base, prev = find_path(start_pos, end_pos)
+    path = get_path(prev, end_pos)
 
-    best_cases = []
-    for y, row in enumerate(input_data[1:-1], 1):
-        for x, col in enumerate(row[1:-1], 1):
-            if col != "#":
+    useful_cheats = Counter()
+    for pos in reversed(path):
+        y, x = pos
+        # Get all positions when moving at most `cheat_size` steps in any direction
+        new_pos_cheat_size = [
+            ((y + dy, x + dx), abs(dy) + abs(dx))
+            for dy in range(-cheat_size, cheat_size + 1)
+            for dx in range(-cheat_size, cheat_size + 1)
+            if abs(dy) + abs(dx) <= cheat_size and (y + dy, x + dx) in path
+        ]
+        for cheat_pos, size in new_pos_cheat_size:
+            if cheat_pos not in path:
                 continue
-            input_data[y][x] = "."
-            dist, _ = find_path(start_pos, end_pos)
-            if base_line > dist[end_pos]:
-                best_cases.append(base_line - dist[end_pos])
-            input_data[y][x] = "#"
+            better_steps = dist_base[cheat_pos] - (dist_base[pos] + size)
+            if better_steps >= threshold:
+                useful_cheats.update([better_steps])
+                # break
 
-    return sum(1 for case in best_cases if case >= threshold)
+    print(useful_cheats)
+    return useful_cheats.total()
 
 
-def part_2(input_file: str):
-    global input_data
-    data_file = Path(__file__).with_name(input_file).read_text()
-    input_data = data_file.split("\n")
+def get_path(prev, end_pos):
+    path = []
+    while end_pos in prev:
+        path.append(end_pos)
+        end_pos = prev[end_pos]
+    return path
 
 
 if __name__ == "__main__":
     print("#" * 10 + " Part 1 " + "#" * 10)
-    result_ex = part_1("input_ex.txt", 12)
+    result_ex = part_1("input_ex.txt", threshold=12)
     print(result_ex)
     assert result_ex == 8
 
-    result = part_1("input.txt")
+    result = part_1("input.txt", threshold=100)
     print(result)
 
     # #### Part 2 ####
     print("#" * 10 + " Part 2 " + "#" * 10)
-    result = part_2("input_ex.txt")
+    result = part_1("input_ex.txt", threshold=70, cheat_size=20)
     print(result)
-    assert result == 1337
+    assert result == 41
 
-    result = part_2("input.txt")
+    result = part_1("input.txt", threshold=100, cheat_size=20)
     print(result)
