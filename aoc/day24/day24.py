eaf859e1297e6739df10798dfd474467061268e3
HansBambel <Kevin.Trebing@gmx.net>
2024-12-25 12:21:36 +0100

    Day 24 - part 2 still wrong

--- aoc/day24/day24.py
+++ aoc/day24/day24.py
@@ -1,8 +1,20 @@
+from itertools import combinations
 from pathlib import Path
 from operator import and_, or_, xor
 
 input_data = []
 connections = []
+operators = {"AND": and_, "OR": or_, "XOR": xor}
+
+
+def get_bin_number(gates, starting_with="z") -> int:
+    bin_number = {
+        name: str(int(value))
+        for name, value in gates.items()
+        if name.startswith(starting_with)
+    }
+    bin_number = dict(sorted(bin_number.items(), key=lambda x: x[0]))
+    return int("".join(bin_number.values())[::-1], 2)
 
 
 def part_1(input_file: str):
@@ -27,17 +39,109 @@ def part_1(input_file: str):
         gates[connection[-1]] = operator(gates[connection[0]], gates[connection[2]])
 
     # Sort the gates by name and convert the binary number to decimal
-    bin_number = {
-        name: str(int(value)) for name, value in gates.items() if name.startswith("z")
-    }
-    bin_number = dict(sorted(bin_number.items(), key=lambda x: x[0]))
-    return int("".join(bin_number.values())[::-1], 2)
+    number = get_bin_number(gates, starting_with="z")
+    return number
 
 
-def part_2(input_file: str):
+def check_addition_correct(gates, connections) -> bool:
+    while len(connections) > 0:
+        connection = connections.pop(0)
+        if gates.get(connection[0]) is None or gates.get(connection[2]) is None:
+            connections.append(connection)
+            continue
+        operator = operators[connection[1]]
+        gates[connection[-1]] = operator(gates[connection[0]], gates[connection[2]])
+
+    y_number = get_bin_number(gates, starting_with="y")
+    x_number = get_bin_number(gates, starting_with="x")
+    result = get_bin_number(gates, starting_with="z")
+    correct = y_number + x_number == result
+    return correct
+
+
+def part_2(input_file: str) -> str:
     global input_data
     data_file = Path(__file__).with_name(input_file).read_text()
-    input_data = data_file.split("\n")
+    input_data, connections = data_file.split("\n\n")
+    input_data = input_data.split("\n")
+    connections = connections.split("\n")
+
+    gates = {
+        name: bool(int(value))
+        for name, value in (gate.split(": ") for gate in input_data)
+    }
+    connections = [connection.split(" ") for connection in connections]
+    # swap the output of 4 connections
+    for swapped_1, swapped_2, swapped_3, swapped_4 in combinations(
+        range(len(connections)), 4
+    ):
+        swapped = connections.copy()
+        to_swap = {
+            swapped[swapped_1][-1],
+            swapped[swapped_3][-1],
+            swapped[swapped_2][-1],
+            swapped[swapped_4][-1],
+        }
+        print(to_swap)
+        swapped[swapped_1][-1], swapped[swapped_3][-1] = (
+            swapped[swapped_3][-1],
+            swapped[swapped_1][-1],
+        )
+        swapped[swapped_2][-1], swapped[swapped_4][-1] = (
+            swapped[swapped_4][-1],
+            swapped[swapped_2][-1],
+        )
+        if check_addition_correct(gates, swapped.copy()):
+            return ",".join(
+                sorted(
+                    [
+                        swapped[swapped_1][-1],
+                        swapped[swapped_2][-1],
+                        swapped[swapped_3][-1],
+                        swapped[swapped_4][-1],
+                    ]
+                )
+            )
+        swapped = connections.copy()
+        swapped[swapped_1][-1], swapped[swapped_4][-1] = (
+            swapped[swapped_4][-1],
+            swapped[swapped_1][-1],
+        )
+        swapped[swapped_2][-1], swapped[swapped_3][-1] = (
+            swapped[swapped_3][-1],
+            swapped[swapped_2][-1],
+        )
+        if check_addition_correct(gates, swapped.copy()):
+            return ",".join(
+                sorted(
+                    [
+                        swapped[swapped_1][-1],
+                        swapped[swapped_2][-1],
+                        swapped[swapped_3][-1],
+                        swapped[swapped_4][-1],
+                    ]
+                )
+            )
+        swapped = connections.copy()
+        swapped[swapped_1][-1], swapped[swapped_2][-1] = (
+            swapped[swapped_2][-1],
+            swapped[swapped_1][-1],
+        )
+        swapped[swapped_3][-1], swapped[swapped_4][-1] = (
+            swapped[swapped_4][-1],
+            swapped[swapped_3][-1],
+        )
+        if check_addition_correct(gates, swapped.copy()):
+            return ",".join(
+                sorted(
+                    [
+                        swapped[swapped_1][-1],
+                        swapped[swapped_2][-1],
+                        swapped[swapped_3][-1],
+                        swapped[swapped_4][-1],
+                    ]
+                )
+            )
 
 
 if __name__ == "__main__":
@@ -54,9 +158,9 @@ if __name__ == "__main__":
 
     # #### Part 2 ####
     print("#" * 10 + " Part 2 " + "#" * 10)
-    result_ex = part_2("input_ex.txt")
+    result_ex = part_2("input_ex3.txt")
     print(result_ex)
-    assert result_ex == 1337
+    assert result_ex == "z00,z01,z02,z05"
 
     result = part_2("input.txt")
     print(result)
